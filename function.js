c10cda618d0f1eb6c70cddc13a35a4826dcae9e1
Anusha24-cre <anushagowdaa30@gmail.com>
2025-01-21 17:39:55 +0530

    changes

---
+++ function.js
@@ -0,0 +1,62 @@
+//function statement
+function b() {
+    console.log('hi');
+}       
+b();
+
+
+
+//function expression
+var a= function() {
+    console.log('Hi Tony');
+}
+
+a();
+
+
+
+//function declaration  are hoisted
+
+b();
+function b() {
+    console.log('hi');
+}
+
+
+//ananymous function 
+
+function s() {
+    console.log('hi');
+}
+// a function without a name is anaonymous function
+
+
+//named function expression
+ function a() {
+    console.log('hi');  
+}
+//a function with a name is named function expression
+
+
+ //parameters and arguments
+    function sayHello(name) {
+        console.log('Hello '+ name);
+    }   
+    sayHello('Tony');
+
+    //output: Hello Tony
+
+
+//default parameters
+    function sayHello(name='Tony') {
+        console.log('Hello '+ name);
+    }
+    sayHello();
+
+    //output: Hello Tony
+
+
+
+
+
+
